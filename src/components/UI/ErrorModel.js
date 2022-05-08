b4cbb6d40683c74b14ce8863bfcd402c0148258f
lcyin <laichunyinkevin@gmail.com>
2022-05-08 16:18:31 +0800

    Implement React Portal

--- src/components/UI/ErrorModel.js
+++ src/components/UI/ErrorModel.js
@@ -1,23 +1,45 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModel.module.css';
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfrim} />;
+};
+
+const ModelOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfrim}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModel = (props) => {
   return (
-    <div className={classes.backdrop} onClick={props.onConfrim}>
-      <Card className={classes.model}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfrim}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfrim={props.onConfrim} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModelOverlay
+          title={props.title}
+          message={props.message}
+          onConfrim={props.onConfrim}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 };
 
